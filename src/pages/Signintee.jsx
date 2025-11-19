94812a8000aae94bc4679a78c73a3e1e68ae5212
WAVY91 <ajakaiyetomisin@gmail.com>
2025-11-19 12:49:34 +0100

    coll

--- src/pages/Signintee.jsx
+++ src/pages/Signintee.jsx
@@ -17,22 +17,29 @@ const Signintee = () => {
         return;
     }
 
-        axios.post("https://back-schema.onrender.com/user/signin", formData)
-        .then((res) => {
-        if (res.data.success === true) {
-            alert("Login successful!");
+axios.post("https://back-schema.onrender.com/user/signin", formData)
+  .then((res) => {
+    if (res.data.success) {
+      alert("Login successful!");
+      localStorage.setItem("token", res.data.token);
+      navigate("/dashboardtee", { state: { user: res.data.user } });
+    } else {
+      alert(res.data.message || "Login failed");
+    }
+  })
+  .catch((err) => {
+    console.log("LOGIN ERROR:", err.response ? err.response.data : err.message);
 
-        setFormData({email: "", password: "", });
-
-        localStorage.setItem("token", res.data.token);
-            navigate("/dashboardtee", {state: {user: res.data.user}});
-        } else {
-            alert(res.data.message || "Login failed");
-        }
-        })
-        .catch((err) => {
-        alert("Invalid credentials. Try again.");
-        });
+    if (err.response?.status === 401) {
+      alert("Invalid email or password");
+    } else if (err.response?.status === 500) {
+      alert("Server error. Try again later.");
+    } else if (err.code === "ERR_NETWORK") {
+      alert("Network error: Backend is unreachable");
+    } else {
+      alert("Something went wrong");
+    }
+  });
     };
 
     return (
